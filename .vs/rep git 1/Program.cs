c8c0e83660f5e9a59f857bb896382aa118ed30e2
adrian sidorencu <sidorencuadi6@gmail.com>
2021-11-25 21:05:06 +0200

    aproape bine

--- .vs/rep git 1/Program.cs
+++ .vs/rep git 1/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Text;
 
 namespace rep_git_1
 {
@@ -7,6 +8,42 @@ namespace rep_git_1
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World! sidorencu Adrian ");
+            string myString = "hello";
+            string myNestString = "world";
+            string a = "a";
+            string b = "b";
+            string c = "c";
+            string random = "B";
+
+            int result = string.Compare(a, b);
+            int resultLeft = string.Compare(a, b);
+            int resultright = string.Compare(c, b);
+            int resultRandom = string.Compare(random, b, false);
+
+            //concatonari
+            myString = myString + "worlds";
+            string hellowu = myString + "my g " + myNestString;
+            Stopwatch stopWatch = StopWatch.StartNew();
+
+
+            for(int i = 0; i<1000; i++)
+            {
+                myString += i;
+
+            }
+
+            StringBuilder builder = new StringBuilder();
+            builder.Append(myString);
+            for (int i = 0; i < 10000; i++)
+            {
+                builder.Append(i);
+
+            }
+            
+
+
+
         }
     }
 }
+
